3f4cbe85f50873c429138ec0eaa4575c473dbdff
omkarmane05 <omkarmane512@gmail.com>
2026-04-02 00:27:05 +0530

    fix production white screen

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Polyfill for PeerJS and WebRTC in production
+if (typeof global === 'undefined') {
+  (window as any).global = window;
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
